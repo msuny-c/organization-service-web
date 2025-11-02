df068b35498103fd466cd6a13e3b5f6370b7fa02
Байрамгулов Мунир Ниязович <mbrmglv@mts.ru>
2025-11-02 15:49:24 +0300

    test api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:35000';
+const API_BASE_URL = 'https://9822f7b10732.ngrok-free.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
