bd35612f70dbbbbe442b844fbc6e5559ec8e208a
Байрамгулов Мунир Ниязович <mbrmglv@mts.ru>
2025-11-03 16:19:59 +0300

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -73,7 +73,7 @@ export default function Layout() {
       </nav>
 
       <main className="flex-1">
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
           <Outlet />
         </div>
       </main>
