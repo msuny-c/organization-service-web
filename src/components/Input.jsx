2f6510d4e3814feded5303361b09bc933a609dfa
Байрамгулов Мунир Ниязович <mbrmglv@mts.ru>
2025-11-30 00:19:06 +0300

    update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,15 +5,6 @@ export default function Input({
   className = '',
   ...props 
 }) {
-  const handleKeyPress = (e) => {
-    if (props.type === 'number') {
-      const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '-', 'Backspace', 'Delete', 'Tab', 'ArrowLeft', 'ArrowRight'];
-      if (!allowedKeys.includes(e.key)) {
-        e.preventDefault();
-      }
-    }
-  };
-
   const handleInput = (e) => {
     if (props.type === 'number') {
       let value = e.target.value.replace(/[^0-9.,-]/g, '');
@@ -50,7 +41,6 @@ export default function Input({
         className={`w-full px-3 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${
           error ? 'border-red-500' : 'border-gray-300'
         }`}
-        onKeyPress={handleKeyPress}
         onInput={handleInput}
         {...props}
       />
